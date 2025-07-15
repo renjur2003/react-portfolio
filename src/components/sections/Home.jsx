1c0c02a878842adc6d2da821f6c3b627a05323e3
renjur2003 <renju.stackuponline@gmail.com>
2025-07-15 14:24:55 +0530

    new commit

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -6,7 +6,8 @@ export const Home = () => {
             <RevealOnScroll>
             <div className="text-center z-10 px-4">
             <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">
-                Hi, I'm Pedro Tech </h1>
+                Hi, I'm Renju R
+                 </h1>
 
                 <p className ="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
                     I am a fullstack developer with a passion for creating dynamic and responsive web applications.
